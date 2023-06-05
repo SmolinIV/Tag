64adb44e641850b281d124ca434e890a679594a0
SmolinIV <Smolin1992@bk.ru>
2023-06-05 16:12:22 +0500

    Added Shared_ptr

--- Menu.h
+++ Menu.h
@@ -2,17 +2,20 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <vector>
+#include <memory>
 #include "Constants.h"
 #include "Tablet.h"
+
 
 class Menu {
 private:
 
-	std::vector<Tablet*> m_menu;
+	std::vector<std::shared_ptr<Tablet>> m_menu;
 	sf::Vector2f m_pos;
 
 	float m_step = 80;
 	int selected_point = 0;
+
 
 public:
 
@@ -25,13 +28,7 @@ public:
 	void moving_up();
 
 
-	~Menu() {
-		for (int i = 0; i < m_menu.size(); i++)
-		{
-			delete m_menu[i];
-			m_menu[i] = nullptr;
-		}
-	}
+	~Menu() {}
 };
 
 Menu::Menu(sf::RenderWindow& window, std::initializer_list <sf::String> list) {
@@ -39,7 +36,7 @@ Menu::Menu(sf::RenderWindow& window, std::initializer_list <sf::String> list) {
 	m_pos.y = window.getSize().y / 2.5;
 	m_menu.reserve(list.size());
 	for (int i = 0; i < list.size(); i++) {
-		Tablet* t = new Tablet{ *(list.begin() + i), {m_pos.x, m_pos.y + m_step * i} };
+		std::shared_ptr<Tablet> t{std::make_shared<Tablet>( *(list.begin() + i), sf::Vector2f{m_pos.x, m_pos.y + m_step * i} ) };
 		m_menu.push_back(t);
 		if (i == selected_point) {
 			m_menu[i]->selected(sf::Color(ORANGE_RED));
@@ -50,7 +47,7 @@ Menu::Menu(sf::RenderWindow& window, std::initializer_list <sf::String> list) {
 }
 
 void Menu::draw(sf::RenderWindow& window) {
-	for (auto item : m_menu) {
+	for (auto &item : m_menu) {
 		(*item).draw(window);
 	}
 }
